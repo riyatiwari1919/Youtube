c47d9b2792f8d4adf64f4947c4c26c70dd810692
riyatiwari1919 <98682707+riyatiwari1919@users.noreply.github.com>
2023-08-27 12:06:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-icon").addEventListener("click", function(){
+    document.querySelector(".content").classList.toggle("visible");
+    document.querySelector(".side-content").classList.toggle("hidden");
+})
+
+/*document.querySelector(".small-screen-menu").addEventListener("click", function(){
+    document.querySelector(".content").classList.toggle("visible");
+})*/
